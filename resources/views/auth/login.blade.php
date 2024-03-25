c757b28a8c60bf8bf710056e53d12457ab60b95a
AyoubMotei <ayoub.motei@gmail.com>
2024-03-25 05:01:48 +0100

    setup the registration page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 
 
-<section style="margin-top: -24px;" class="section-hero overlay inner-page bg-image" style="background-image: url('{{asset('assets/images/hero_1.jpg') }}');" id="home-section">
+<section  class="section-hero overlay inner-page bg-image" style="margin-top: -24px; background-image: url({{asset('assets/images/hero_1.jpg') }});" id="home-section">
       <div class="container">
         <div class="row">
           <div class="col-md-7">
