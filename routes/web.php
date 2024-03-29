f6e2e4a259f466fed8b1dcde31b4fe9d4f31617b
AyoubMotei <ayoub.motei@gmail.com>
2024-03-29 03:14:41 +0100

    Fix Categories errors

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::post('/jobs/save', [App\Http\Controllers\Jobs\JobsController::class, 'sav
 
 Route::post('/jobs/apply', [App\Http\Controllers\Jobs\JobsController::class, 'jobApply'])->name('apply.job');
 
+
+Route::get('/categories/single/{name}', [App\Http\Controllers\Categories\CategoriesController::class, 'singleCategory'])->name('categories.single');
+
+
