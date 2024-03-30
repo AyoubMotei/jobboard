c259340b0e7a2b429481debbf1267820c634f57b
AyoubMotei <ayoub.motei@gmail.com>
2024-03-31 00:35:16 +0100

    Add Creating New Admin feature

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,10 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function(){
    
     Route::get('/all-admins', [App\Http\Controllers\Admins\AdminsController::class, 'admins'])->name('view.admins');
 
+    Route::get('/create-admins', [App\Http\Controllers\Admins\AdminsController::class, 'createAdmins'])->name('create.admins');
+
+    Route::post('/create-admins', [App\Http\Controllers\Admins\AdminsController::class, 'storeAdmins'])->name('store.admins');
+
    
     
 });
