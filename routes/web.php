c488e7c4f3a2ea30bf7a36ca452d6466c98fc928
AyoubMotei <ayoub.motei@gmail.com>
2024-03-31 16:26:11 +0200

    Add Deleting Categories feature

--- routes/web.php
+++ routes/web.php
@@ -92,4 +92,8 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function(){
     Route::get('/edit-cates/{id}', [App\Http\Controllers\Admins\AdminsController::class, 'editCategories'])->name('edit.categories');
     Route::post('/edit-cates/{id}', [App\Http\Controllers\Admins\AdminsController::class, 'updateCategories'])->name('update.categories');
 
+
+
+    Route::get('/delete-cates/{id}', [App\Http\Controllers\Admins\AdminsController::class, 'deleteCategories'])->name('delete.categories');
+
 });
